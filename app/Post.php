6a8872464c0df9027f39f7613f39ccff62bd03e2
Callie Briscoe <calliebriscoe@gmail.com>
2016-03-31 23:53:51 -0500

    added model for post in how it interacts with users, subbreddits, and subscribedusers

--- app/Post.php
+++ app/Post.php
@@ -1,33 +1,27 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-
-class Post extends Model
+class Subbreddit extends Model
 {
-  /**
-  * Get the user that owns the post.
-  */
-  public function user()
-   {
-       return $this->belongsTo('App\User');
-   }
-
-   /**
-   * Get the subreddit that owns the comment.
-   */
-   public function subreddit()
+     /**
+     * Get the user that owns the subbreddit.
+     */
+    public function user()
     {
-        return $this->belongsTo('App\Subreddit');
+        return $this->belongsTo('App\User');
     }
-
-
+     /**
+     * Get the posts of the subbreddit.
+     */
+    public function subbreddit()
+    {
+        return $this->hasMany('App\Subbreddit');
+    }
     /**
-    * Get the comments the user has created.
-    */
-    public function comments()
-     {
-         return $this->hasMany('App\Commit');
-      }
+     * Get the subscribed users of the subbreddit.
+     */
+    public function subscribedUsers()
+    {
+        return $this->belongsToMany('App\User')->withTimestamps();
+    }
 }
