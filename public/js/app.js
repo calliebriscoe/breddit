499c9f975a79472fce9a85d3a80bdd8f67441641
Callie Briscoe <calliebriscoe@gmail.com>
2016-04-01 23:30:20 -0500

    added base to start working with Backbone.JS

---
+++ public/js/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
+= subbreddit.name;
+var PostModel = Backbone.Model.extend({
+		= ' ';
+		urlRoot: '/api/posts/',
+		idAttribute: 'id',
+});
+
+ var PostCollection = Backbone.Collection.extend({
+ 	url: '/api/posts/',
+ 	model: PostModel
+ });
+
+ var PostItemView = Backbone.View.extend({
+ 	el:'<div></div>',
+
+ 	template: _.template('<h2><%= post.get("title") %></h2>'),
+
+ 	render: function() {
+ 		this.$el.html(this.template({ post: this.model }));
+  	}		  	}
+ -}); 		 });
+
+ var post = new PostModel({id: 1});
+
+ post.fetch({
+ 	success: function() {
+ 		var postItemView = new PostItemView({ model: post });
+ 		postItemView.render();
+
+ 		$('#content').html(postItemView.el);
+ 	}
+ });
